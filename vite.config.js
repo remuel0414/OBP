7795f687bb2b657587c77c8eb09f8d109fa483c7
remuel144 <146079776+remuel144@users.noreply.github.com>
2024-04-16 15:05:40 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/https://github.com/remuel0414/OBP.git/', // Replace 'your-repo-name' with your repo name
+  base: '/OBP/', // Replace 'your-repo-name' with your repo name
   plugins: [react()],
   server: {
     port: 3000,
